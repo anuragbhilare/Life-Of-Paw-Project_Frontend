96107aed2bb710c5a784a4edb7ec8d4b859493c0
Anurag Bhilare <anuragbhilare8@gmail.com>
2026-07-09 18:16:33 +0530

    Configure application for production deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:9999/api';
+const BASE_URL = 'https://life-of-paw-project-backend.onrender.com/api';
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
